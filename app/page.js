49bf686b0174945bd32bb05a4accc089a85947d7
Sanket Banerjee <sanketbanerjee.2004@gmail.com>
2024-12-10 23:08:24 +0530

    disabling eslint

--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ import Footer from "./components/Footer";
 
 export default function Home() {
   return (
-    <main className="">
+    <main>
       <Navbar />
       <HomePage />
       <Footer />
--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     domains: ["avatars.githubusercontent.com"], // Add the allowed domain here
   },
+  eslint: {
+    ignoreDuringBuilds: true, // Disables all ESLint errors during builds
+  },
 };
 
 export default nextConfig;
